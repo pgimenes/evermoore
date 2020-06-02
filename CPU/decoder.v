f8832d87fe4bb32aafc5f5e63ad8a8158d6c7d4a
Pedro Gimenes <pgimenes@outlook.com>
2020-06-02 19:49:37 +0100

    merged decoders

--- CPU/decoder.v
+++ CPU/decoder.v
@@ -2,9 +2,10 @@ module Decoder
 
 (
 
-input instruction[15:0];
-input state[1:0];
-output alu_input_sel, reg_data1_sel, reg_data2_sel, reg_shift_en, reg_shiftin, ram_instr_addr_sel, ram_data_addr_sel, ir_mux, jump_sel, status_reg_sload, pc_sload, pc_cnt_en, ir_en, ram_wren_instr, ram_wren_data, sm_extra, decoder_opcode[5:0]
+input [15:0] instruction,
+input [1:0] state,
+output [5:0] encoded_opcode,
+output alu_input_sel, reg_data1_sel, reg_data2_sel, reg_shift_en, reg_shiftin, ram_instr_addr_sel, ram_data_addr_sel, ir_mux, jump_sel, status_reg_sload, pc_sload, pc_cnt_en, ir_en, ram_wren_instr, ram_wren_data, sm_extra
 );
 
 wire fetch, exec1, exec2;
@@ -12,8 +13,6 @@ wire fetch, exec1, exec2;
 assign fetch = ~state[0]&~state[1];
 assign exec1 = ~state[0]&state[1];
 assign exec2 = state[0]&~state[1];
-
-
 
 wire jmr, car, lsr, asr, inv, twc, inc, dec, ldi, aim, sim, seb, clb ,stb ,lob, add, adc, sub, sbc, gha, ghs, mov, mow, push, load, pop, store, AND, OR, XOR, comp, mul, mls, jmd, call, lda, rtn, stp, clear, sez, clz, sen, cln, sec, clc, set, clt, sev, clv, ses, cls, sei, cli, bru, brd ;
 
@@ -39,30 +38,6 @@ assign store= ~instruction[15]&instruction[14]&instruction[13]&~instruction[12]&
 
 assign mul 	= instruction[15]&~instruction[14]&~instruction[13];
 
+assign sm_extra = (lda&exec1)||(aim&exec1)||(sim&exec1) ;
 
-
-
-
-
-
-
-
-
-
-
-
-
-assign sm_extra = (lda&exec1)||(adi&exec1)||(sim&exec1) ;
-
-
-
-
-
-
-
-
-
-
-
-
-
+endmodule
