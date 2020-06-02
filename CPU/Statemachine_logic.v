8ef0aca31db572fd8f52840495cf4a0142709ef2
Gergely Kovacs <gbk19@ic.ac.uk>
2020-06-02 10:22:40 +0200

    Added Statemachine_logic module

---
+++ CPU/Statemachine_logic.v
@@ -0,0 +1,10 @@
+module Statemachine_logic (
+	input EXTRA, 		//1 for 3 cycle instruction
+	input [1:0] Q,		//previous state
+	output [1:0] N		//next state
+);
+
+	assign N[1] = ~Q[1]&Q[0]&EXTRA;
+	assign N[0] = (~Q[1]&~Q[0])|(~Q[1]&Q[0]&~EXTRA)|(Q[1]&~Q[0]);
+
+endmodule
